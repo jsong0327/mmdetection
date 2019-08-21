e9bf72ef038cdacbfeea712bde6705372623d104
Jaeyub Song <jsong0327@kaist.ac.kr>
2019-08-21 02:55:30 +0000

    Moved files to user_tools folder

--- user_tools/add_dataset_to_mongo.py
+++ user_tools/add_dataset_to_mongo.py
@@ -9,8 +9,8 @@ if len(sys.argv) != 3 or sys.argv[1] != '-folder':
 if sys.argv[2][-1] != '/':
   sys.argv[2] = sys.argv[2] + '/'
 
-config_file = 'configs/htc/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e.py'
-checkpoint_file = 'checkpoints/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e_20190408-0e50669c.pth'
+config_file = '../configs/htc/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e.py'
+checkpoint_file = '../checkpoints/htc_dconv_c3-c5_mstrain_400_1400_x101_64x4d_fpn_20e_20190408-0e50669c.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
